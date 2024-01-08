8122711a3c61f6689534df701b4547c6f6ad66eb
KipMi <othellogy@gmail.com>
2024-01-08 07:50:12 +0700

    update auth 2

--- api/src/controllers/user.controller.js
+++ api/src/controllers/user.controller.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const prisma = require('../db');
-const jwt = require('jsonwebtoken');
-const { authenticate } = require('../middlewares/authMiddleware');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const prisma = require("../db");
+const jwt = require("jsonwebtoken");
+const { authenticate } = require("../middlewares/authMiddleware");
 const {
   getAllUsers,
   getUserById,
@@ -11,18 +11,18 @@ const {
   editUserById,
   patchUserById,
   getUserByUsername,
-} = require('../services/user.service');
+} = require("../services/user.service");
 const secretKey = process.env.SECRET_KEY;
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   const user = await getAllUsers();
 
   res.send(user);
 });
 
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     const userId = req.params.id;
     const user = await getUserById(userId);
@@ -33,7 +33,7 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const newUserData = req.body;
 
@@ -41,14 +41,14 @@ router.post('/', async (req, res) => {
 
     res.status(201).send({
       data: user,
-      message: 'New user created',
+      message: "New user created",
     });
   } catch (error) {
     res.status(400).send(error.message);
   }
 });
 
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     const { username, password } = req.body;
 
@@ -57,7 +57,7 @@ router.post('/login', async (req, res) => {
     if (!user || !bcrypt.compareSync(password, user.password)) {
       return res
         .status(401)
-        .json({ error: 'Invalid credentials', user: user.password });
+        .json({ error: "Invalid credentials", user: user.password });
     }
 
     const token = jwt.sign(
@@ -68,25 +68,25 @@ router.post('/login', async (req, res) => {
         role: user.role,
       },
       secretKey,
-      { expiresIn: '1h' }
+      { expiresIn: "1h" }
     );
 
-    res.cookie('token', token, { httpOnly: true });
+    res.cookie("token", token, { httpOnly: false });
     res.json({ success: true });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-router.post('/logout', (req, res) => {
-  res.clearCookie('token');
+router.post("/logout", (req, res) => {
+  res.clearCookie("token");
   res.json({ success: true });
 });
 
-router.get('/get/user', authenticate, (req, res) => {
+router.get("/get/user", authenticate, (req, res) => {
   if (!req.user) {
-    return res.status(401).send({ error: 'Unauthorized' });
+    return res.status(401).send({ error: "Unauthorized" });
   }
 
   const userData = {
@@ -98,39 +98,39 @@ router.get('/get/user', authenticate, (req, res) => {
   res.json(req.user);
 });
 
-router.get('/token', (req, res) => {
+router.get("/token", (req, res) => {
   // const token = req.cookies.token;
-  res.json({ message: 'hello' });
+  res.json({ message: "hello" });
   // console.log(token);
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
     const userId = req.params.id;
     await deleteUserById(userId);
-    res.send('User deleted');
+    res.send("User deleted");
   } catch (error) {
     res.status(400).send(error.message);
   }
 });
 
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
   const userId = req.params.id;
   const userData = req.body;
 
   if (!(userData.username && userData.password)) {
-    res.status(400).send('Some fields are missing');
+    res.status(400).send("Some fields are missing");
   }
 
   const user = await editUserById(userId, userData);
 
   res.send({
     data: user,
-    message: 'User updated',
+    message: "User updated",
   });
 });
 
-router.patch('/:id', async (req, res) => {
+router.patch("/:id", async (req, res) => {
   try {
     const userId = req.params.id;
     const userData = req.body;
@@ -139,7 +139,7 @@ router.patch('/:id', async (req, res) => {
 
     res.send({
       data: user,
-      message: 'User updated',
+      message: "User updated",
     });
   } catch (error) {
     res.status(400).send(error.message);
