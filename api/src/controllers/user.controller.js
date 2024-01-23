1311414e8ac4fc65092f40045b5a0b011720a4b0
KipMi <othellogy@gmail.com>
2024-01-23 15:56:11 +0700

    update auth

--- api/src/controllers/user.controller.js
+++ api/src/controllers/user.controller.js
@@ -73,7 +73,7 @@ router.post("/login", async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "none",
+      sameSite: "lax",
       secure: true,
     });
     res.json({ success: true });
