d6b366c0a9dc2060164c46735b547daa3a05ba69
KipMi <othellogy@gmail.com>
2024-01-24 13:10:03 +0700

    edit new

--- app/pelayanan/baptis/page.tsx
+++ app/pelayanan/baptis/page.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import BaptisCard from './BaptisCard'
+import React from "react";
+import BaptisCard from "./BaptisCard";
 
 const BaptisPage = () => {
   return (
     <div>
-      <BaptisCard category='Baptis Anak' jadwal='29 Mei 16:00'>Informasi Pendaftaran</BaptisCard>
-      <BaptisCard category='Baptis Sidi/Dewasa' jadwal='29 Mei 16:00' jadwalKatekisasi='30 Mei 16:00'>Informasi Pendaftaran</BaptisCard>
+      <BaptisCard category="Baptis Anak" jadwal="29 Mei 16:00"></BaptisCard>
+      <BaptisCard
+        category="Baptis Sidi/Dewasa"
+        jadwal="29 Mei 16:00"
+        jadwalKatekisasi="30 Mei 16:00"
+      ></BaptisCard>
     </div>
-  )
-}
+  );
+};
 
-export default BaptisPage
+export default BaptisPage;
